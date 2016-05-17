7573b7d472865bd247f2e8b454f07dceda71669d
Yury Layeuski <ylayeuski@ylayeuski-mbp.local>
2016-05-16 21:52:18 -0700

    index: get results from db

--- Leprosorium/app.rb
+++ Leprosorium/app.rb
@@ -25,7 +25,10 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	#choosing list of posts from database
+	@results = @db.execute 'select * from Posts order by id desc'
+
+	erb :index
 end
 
 get '/new' do
